f29a780927387b68a5bb32660ac0bb46ea9667c6
Your Name <you@example.com>
2021-08-01 22:07:29 +0300

    commit message

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="sortcut icon" href="img/favicon.ico" type="image/x=icon">
+    <title>Немного о себе</title>
+</head>
+<body style="background-color: #df5d84">
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <img src="изображение_viber_2021-08-01_16-02-18-677.jpg" class="card-img-top" alt="фото"> 
+        </div>
+    </div>
+</header>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col12">
+                <h4>Инфо</h4>
+                <h5> Виктория Горлова</h5>
+                <hr>
+                <div>
+                    <p>Контактая информация</p>
+                    тел. 099 1509795<br>
+                    <em>vicoolgorlova@gmail.com</em></p> 
+                    <hr>
+            </div>
+        </div>
+    </div>
+        
+    <div class="container">
+        <div class="row">
+            <div>
+            <p> О себе:
+                <ul type="disk">
+                <li>занимаюсь спортом,</li> 
+                <li>стараюсь вести здоровый образ жизни,</li>
+                <li>очень любдлю детей.</li></ul></p>
+             </div>
+
+    </div>
+</body>
+</html>
